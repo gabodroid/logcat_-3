5edd4fd71514a443d2188db9f6657819e96c17d0
Gabriel Mrva <gabriel.mrva@aintuinc.com>
2016-12-05 16:48:40 +0100

    Empty UI

--- app/src/main/java/com/jfdimarzio/uicode/MainActivity.java
+++ app/src/main/java/com/jfdimarzio/uicode/MainActivity.java
@@ -16,23 +16,21 @@ public class MainActivity extends AppCompatActivity {
                 new LinearLayoutCompat.LayoutParams(
                         LinearLayoutCompat.LayoutParams.WRAP_CONTENT,
                         LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+
         //---create a layout---
         LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // TODO
+        //---set the orientation---
+
         //---create a textview---
-        TextView tv = new TextView(this);
-        tv.setText("This is a TextView");
-        tv.setLayoutParams(params);
+
         //---create a button---
-        Button btn = new Button(this);
-        btn.setText("This is a Button");
-        btn.setLayoutParams(params);
 
         //---adds the textview---
-        layout.addView(tv);
+
         //---adds the button---
-        layout.addView(btn);
-        //---create a layout param for the layout---
+
         LinearLayoutCompat.LayoutParams layoutParam =
                 new LinearLayoutCompat.LayoutParams(
                         LinearLayoutCompat.LayoutParams.WRAP_CONTENT,
